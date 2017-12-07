4e5847b9a57081604b560be184b08c98c427b48b
atkjs <atikshakil1@gmail.com>
2017-12-07 15:51:59 +0600

    add folder

--- resources/views/AliensPanel/index/index.blade.php
+++ resources/views/AliensPanel/index/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="{{asset('../../../../public/AliensPanel/css/AliensPanel.css')}}">
     <link rel="stylesheet" href="{{asset('../../../../public/AliensPanel/css/bootstrap.css')}}">
     <script src="../../../../public/AliensPanel/js/bootstrap.js"></script>
 </head>
